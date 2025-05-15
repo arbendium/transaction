fdc8b20f3dfa9c6bfcaa9df0ce0c3d66dbe34804
Keijo Kapp <keijo.kapp@gmail.com>
2025-05-15 17:27:49 +0300

    asd

--- lib/types.ts
+++ lib/types.ts
@@ -14,3 +14,24 @@ export type RangeIndexAction =
 	| 'setReadConflict'
 	| 'setWriteConflict'
 	| ['addMutation', Mutation]
+
+export interface Backend {
+	get(key: Buffer, addConflict: boolean): Promise<Buffer | undefined>
+	getRange(
+		start: Buffer,
+		startInclusive: boolean,
+		startOffset: number,
+		end: Buffer,
+		endInclusive: boolean,
+		endOffset: number,
+		limit: number,
+		reverse: boolean,
+		addConflict: boolean
+	): Promise<[Buffer, Buffer][]>
+	getKey(key: Buffer, inclusive: boolean, offset: number, addConflict: boolean): Promise<Buffer>
+	set(key: Buffer, value: Buffer, addConflict: boolean): void
+	clear(key: Buffer, addConflict: boolean): void
+	clearRange(start: Buffer, end: Buffer, addConflict: boolean): void
+	addReadConflictRange(start: Buffer, end: Buffer): void
+	addWriteConflictRange(start: Buffer, end: Buffer): void
+}
